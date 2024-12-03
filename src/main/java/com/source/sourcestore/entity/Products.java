4be9edc070ea0f11cc94c9530474762579de78e9
database_1 <homthucuavinhuchiha@gmail.com>
2024-12-03 19:26:35 +0700

    Create User

---
+++ src/main/java/com/source/sourcestore/entity/Products.java
@@ -0,0 +1,76 @@
+package com.source.sourcestore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String productId;
+    private String productName;
+    private String productPrice;
+    private Long productStock;
+    private String productSize;
+    private String productColor;
+    private String productDescription;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public Long getProductStock() {
+        return productStock;
+    }
+
+    public void setProductStock(Long productStock) {
+        this.productStock = productStock;
+    }
+
+    public String getProductSize() {
+        return productSize;
+    }
+
+    public void setProductSize(String productSize) {
+        this.productSize = productSize;
+    }
+
+    public String getProductColor() {
+        return productColor;
+    }
+
+    public void setProductColor(String productColor) {
+        this.productColor = productColor;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+}
+
